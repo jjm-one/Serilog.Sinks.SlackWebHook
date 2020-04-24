51ee560a2ad6592b6c2f450fd5891248531dc623
Jonas Merkle [JJM] <jonas@jjm.one>
2020-04-24 19:16:02 +0200

    test

---
+++ Serilog.Sinks.SlackWebHook/Serilog.Sinks.SlackWebHook.Tests/SlackSinkOptionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Serilog.Sinks.SlackWebHook.Tests
+{
+    [TestFixture]
+    public class SlackSinkOptionsTests
+    {
+        #region setup
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        #endregion
+
+        #region init tests
+
+        [Test]
+        public void DefaultConstructorTest()
+        {
+            var options = new SlackSinkOptions();
+
+            Assert.IsTrue(options.SlackAttachmentColors.Count == 6);
+            Assert.IsTrue(options.SlackAttachmentFooterIcon.Count == 6);
+
+            Assert.IsTrue(options.PeriodicBatchingSinkOptionsBatchSizeLimit >= 0);
+            Assert.IsTrue(!options.PeriodicBatchingSinkOptionsPeriod.Equals(TimeSpan.Zero));
+            Assert.IsTrue(options.PeriodicBatchingSinkOptionsQueueLimit >= 0);
+        }
+
+        #endregion
+    }
+}
