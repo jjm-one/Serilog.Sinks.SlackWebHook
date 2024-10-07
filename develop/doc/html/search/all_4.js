6321fae10d63f09d1ddd966ed5811119f1974f58
jonas-merkle <jonas@jjm.one>
2024-10-07 15:44:34 +0000

    Deploying to gh-pages from @ jjm-one/jjm.one.Serilog.Sinks.SlackWebHook@583fabc71e8e4fc1c870563742a510a6be6c5dff 🚀

--- develop/doc/html/search/all_4.js
+++ develop/doc/html/search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['emitbatchasync_20',['EmitBatchAsync',['../classSlackSerilogBatchedLogEventSink.html#a103a1183e79d929c246c5473f0b1f8f2',1,'SlackSerilogBatchedLogEventSink']]],
-  ['emitbatchasyncworkscorrectly_21',['EmitBatchAsyncWorksCorrectly',['../classSlackSerilogBatchedLogEventSinkTests.html#a1a77fd38019b2ad8a4be962f651b8b80',1,'SlackSerilogBatchedLogEventSinkTests']]]
+  ['emitbatchasyncworkscorrectly_21',['EmitBatchAsyncWorksCorrectly',['../classSlackSerilogBatchedLogEventSinkTests.html#ad04a9775a0f120c44adc9c9adda094e0',1,'SlackSerilogBatchedLogEventSinkTests']]]
 ];
